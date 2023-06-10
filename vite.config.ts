06970609bee51d53546e579d73f8ed91a3ec2bd3
moebiusmania <moebiusmania@gmail.com>
2023-06-10 10:24:39 +0200

    fix: config starts on port 3000

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ const base: string | undefined =
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+  },
   plugins: [react()],
   base,
 });
